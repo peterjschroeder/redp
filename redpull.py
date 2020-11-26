0ac0c53415d9bd60350a31f7c5b59cb350fed011
Peter J. Schroeder <peterjschroeder@gmail.com>
2020-11-26 03:42:31 -0500

    Temporarly disabled expired check as it's having unwanted results.

--- redpull.py
+++ redpull.py
@@ -204,7 +204,8 @@ def get_submissions(subreddit, use_pushshift):
     retrieved = get_retrieved(subreddit[0])
 
     if use_pushshift:
-        for j in set(list(api.search_submissions(after=max(int(float(subreddit[1])), time.time() - int(subreddit[4])*86400), subreddit=subreddit[0], limit=None))):
+#        for j in set(list(api.search_submissions(after=max(int(float(subreddit[1])), (time.time() - int(subreddit[4])*86400)), subreddit=subreddit[0], limit=None))):
+        for j in set(list(api.search_submissions(after=int(float(subreddit[1])), subreddit=subreddit[0], limit=None))):
             write_messages(j, retrieved, int(float(subreddit[1])), int(subreddit[2]), 
                     int(subreddit[3]), int(subreddit[4])*86400, False)
     else:
@@ -350,14 +351,18 @@ def get_attachment(url, msg):
             headers={'User-Agent': 'Mozilla/5.0'}), timeout=10)
         info = response.info()
         imgdata = response.read()
-    except (urllib.error.HTTPError, socket.timeout) as e:
-        # FIXME: Handle this with wayback machine.
-        if e.code == 404:
+    except:
+        # FIXME: Lets do this without repeating code.
+        try:
+            logging.info("Dead link. Trying on the wayback machine.")
+            url = "http://web.archive.org/web/"+url
+            response = urlopen(Request(url.encode('ascii', errors='ignore').decode(), 
+                headers={'User-Agent': 'Mozilla/5.0'}), timeout=10)
+            info = response.info()
+            imgdata = response.read()
+        except:
+            logging.warning("Link not in the wayback machine. Attachment skipped. Embeding url instead.")
             return False
-        else:
-            return False
-    except Exception:
-        return False
 
     exclude_mimetypes = ["application/x-httpd-php", "text/asp", "text/css", "text/html", "text/javascript"]
     if info.get_content_maintype() in attachments and info.get_content_type() not in exclude_mimetypes:
